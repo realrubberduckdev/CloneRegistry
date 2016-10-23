e092dfd03469d885ceba8645880bfe4051d03aac
Dushyant Priyadarshee <dushyantpriyadarshee@gmail.com>
2016-10-23 20:00:03 +0100

    Add unit tests

--- TestCloneRegistry/TestStringExtensions.cs
+++ TestCloneRegistry/TestStringExtensions.cs
@@ -1,18 +1,46 @@
 ﻿using CloneRegistry;
 using Microsoft.Win32;
 using NUnit.Framework;
+using NUnit.Framework.Constraints;
+using System;
 
 namespace TestCloneRegistry
 {
     [TestFixture]
     public class TestStringExtensions
     {
-        [Test]
-        public void TestParseRegistryKey()
+        [TestCase(null)]
+        [Description("Test that null input throws NRE.")]
+        public void TestParseRegistryKeyNRE(string inputString)
         {
-            string someString = "";
-            RegistryKey keyValueFromString = someString.ParseRegistryKey();
+            Assert.Throws<NullReferenceException>(() => inputString.ParseRegistryKey());
+        }
+
+        [TestCase("")]
+        [TestCase("RandomValue")]
+        [Description("Test that invalid input throws exception.")]
+        public void TestParseRegistryKeyNREException(string inputString)
+        {
+            ActualValueDelegate<RegistryKey> testDelegate = () => inputString.ParseRegistryKey();
+            Assert.That(testDelegate, Throws.TypeOf<Exception>());
+            //TODO: This should be a more specific exception. Fix code and update test.
+        }
+
+        [TestCase(@"HKEY_CLASSES_ROOT\*")]
+        [TestCase(@"HKEY_CLASSES_ROOT\.386")]
+        [TestCase(@"HKEY_CURRENT_USER\AppEvents")]
+        [TestCase(@"HKEY_LOCAL_MACHINE\BCD00000000")]
+        [TestCase(@"HKEY_USERS\S-1-5-18")]
+        [TestCase(@"HKEY_USERS\.DEFAULT")]
+        [TestCase(@"HKEY_CURRENT_CONFIG\System")]
+        [Description("Test that we get the expected value for provided input from ParseRegistryKey method.")]
+        public void TestParseRegistryKey(string inputString)
+        {
+            RegistryKey keyValueFromString = inputString.ParseRegistryKey();
             Assert.That(keyValueFromString, Is.Not.Null);
+
+            string obtainedKeyInString = keyValueFromString.ToString();
+            Assert.That(obtainedKeyInString, Is.EqualTo(inputString), "Invalid value for registry key obtained.");
         }
     }
 }
