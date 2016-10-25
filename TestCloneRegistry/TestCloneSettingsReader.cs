7c982d2e1c2f45fe92336114f16a734c8c97b927
Dushyant Priyadarshee <dushyantpriyadarshee@gmail.com>
2016-10-25 19:07:09 +0100

    Missed in previous commit

---
+++ TestCloneRegistry/TestCloneSettingsReader.cs
@@ -0,0 +1,46 @@
+﻿using CloneRegistry;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestCloneRegistry
+{
+    [TestFixture]
+    public class TestCloneSettingsReader
+    {
+        string _testDataPath;
+        string _settingsFilePath;
+
+        [OneTimeSetUp]
+        public void FixtureOneTimeSetup()
+        {
+            _testDataPath = TestSystemHelper.GetTestDataPath("TestCloneSettingsReader");
+            _settingsFilePath = Path.Combine(_testDataPath, "settings.xml");
+        }
+
+        [Test]
+        public void TestGetCopyData()
+        {
+            List<CopyData> expectedCopyDataList = new List<CopyData>();
+            expectedCopyDataList.Add(new CopyData()
+            {
+                SourceKey = @"HKEY_CURRENT_USER\Console",
+                DestinationKey = @"HKEY_CURRENT_USER\ConsoleBackup"
+            });
+            expectedCopyDataList.Add(new CopyData()
+            {
+                SourceKey = @"HKEY_CURRENT_USER\Environment",
+                DestinationKey = @"HKEY_CURRENT_USER\EnvironmentBackup"
+            });
+
+            var csReader = new CloneSettingsReader(_settingsFilePath);
+            List<CopyData> copyDataList = csReader.GetCopyData();
+
+            Assert.That(copyDataList, Is.EqualTo(expectedCopyDataList), "GetCopyData incorrect CopyData list.");
+        }
+    }
+}
