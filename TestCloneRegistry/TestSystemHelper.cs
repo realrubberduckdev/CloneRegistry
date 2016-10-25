98bdd3b43e26dfa081db0ff26d0249f3ce750ce3
Dushyant Priyadarshee <dushyantpriyadarshee@gmail.com>
2016-10-25 19:09:45 +0100

    Add doc comments

--- TestCloneRegistry/TestSystemHelper.cs
+++ TestCloneRegistry/TestSystemHelper.cs
@@ -3,8 +3,17 @@ using System.IO;
 
 namespace TestCloneRegistry
 {
+    /// <summary>
+    /// Helper methods, constants etc for tests should go in this class.
+    /// </summary>
     public class TestSystemHelper
     {
+        /// <summary>
+        /// Helps tests get a location where they can use already stored data
+        /// to be used for testing.
+        /// </summary>
+        /// <param name="testName"></param>
+        /// <returns></returns>
         public static string GetTestDataPath(string testName)
         {
             string crHomePath = Environment.GetEnvironmentVariable("CRHOME");
