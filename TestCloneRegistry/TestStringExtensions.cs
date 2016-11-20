34476d4542c1658d2123f27faf4ab21b9f961d68
Dushyant Priyadarshee <dushyantpriyadarshee@gmail.com>
2016-11-20 15:07:40 +0000

    Remove tests. Unit tests should not be modifying the registry.

--- TestCloneRegistry/TestStringExtensions.cs
+++ TestCloneRegistry/TestStringExtensions.cs
@@ -25,22 +25,5 @@ namespace TestCloneRegistry
             Assert.That(testDelegate, Throws.TypeOf<Exception>());
             //TODO: This should be a more specific exception. Fix code and update test.
         }
-
-        [TestCase(@"HKEY_CLASSES_ROOT\*")]
-        [TestCase(@"HKEY_CLASSES_ROOT\.386")]
-        [TestCase(@"HKEY_CURRENT_USER\AppEvents")]
-        [TestCase(@"HKEY_LOCAL_MACHINE\BCD00000000")]
-        [TestCase(@"HKEY_USERS\S-1-5-18")]
-        [TestCase(@"HKEY_USERS\.DEFAULT")]
-        [TestCase(@"HKEY_CURRENT_CONFIG\System")]
-        [Description("Test that we get the expected value for provided input from ParseRegistryKey method.")]
-        public void TestParseRegistryKey(string inputString)
-        {
-            RegistryKey keyValueFromString = inputString.ParseRegistryKey();
-            Assert.That(keyValueFromString, Is.Not.Null);
-
-            string obtainedKeyInString = keyValueFromString.ToString();
-            Assert.That(obtainedKeyInString, Is.EqualTo(inputString), "Invalid value for registry key obtained.");
-        }
     }
 }
