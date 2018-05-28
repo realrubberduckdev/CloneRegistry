cb9756825e2bda38ae45964d68c5413689d66cde
Dushyant Priyadarshee <dushyantpriyadarshee@gmail.com>
2018-05-28 10:02:40 +0100

    Use Nuget to get NUnit and use it to run tests.

--- setenv.bat
+++ setenv.bat
@@ -3,7 +3,7 @@ rem Clone Registry (CR) environment setup
 set CRHOME=%cd%
 
 rem NUNIT_HOME is created during build process
-set NUNIT_HOME=%CRHOME%\packages\NUnit.3.10.1\lib\net45
+set NUNIT_TOOLS_HOME=%CRHOME%\packages\NUnit.ConsoleRunner.3.8.0\tools
 
 set NUGET_HOME=%cd%\CRNuGet
 set PATH=%NUNIT_HOME%;%NUGET_HOME%;%PATH%
